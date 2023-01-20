11b2d5aee18e4f76d1a98e4162c58aa66eeeaa26
Kutman73 <0.0000000002clash@gmail.com>
2023-01-20 23:24:30 +0600

    file check added

--- anime/fileschek.py
+++ anime/fileschek.py
@@ -1,29 +1,112 @@
+import hashlib
 import os
 from django.core.exceptions import ValidationError
 from django.core.files.storage import FileSystemStorage
 from api_anime01 import settings
 
 
-def check_file_anime_cover_size(file_object) -> None:
+def file_hashing(instance, file: str, field: str) -> str:
+    hash256 = hashlib.sha256()
+    format_file = os.path.splitext(file)[1].lower()
+    field_file = getattr(instance, field)
+    for byte_chunk in field_file.chunks():
+        hash256.update(byte_chunk)
+    encrypted_name_of_the_anime = hash256.hexdigest()
+    return f"{encrypted_name_of_the_anime + format_file}"
+
+
+def file_exist_check(path: str) -> bool or int:
+    """
+    Receives
+        'path'
+            type: str
+    Return
+        False if a file exists at this path
+        else return 'path'
+    """
+    if os.path.isfile(path):
+        return False
+    return path
+
+
+def check_file_anime_cover_size(file_object) -> ValidationError or None:
+    """
+    receives
+        'file_object'
+            type: django.db.models.fields.files.ImageFieldFile
+    return
+        ValidationError if file size > 'file_limit'
+    """
     file_limit = 3  # field is a Byte
     if file_object.size > file_limit * 1024 * 1024:
         raise ValidationError(f"Max size file {file_limit}MB")
 
 
 def get_path_to_cover_anime(instance, file) -> str:
-    """ The path to the file
-        Path format: media/(original_anime_name)/cover/(photo).png
     """
-    return f"{instance.original_anime_name}/cover/{file}"
+    receives
+        'instance' is a class model
+            type: class(model class instance)
+        'file' is a filename to customer
+            type: str(filename)
+    manipulation data
+        checked for file existence, if file is not exist file add
+    return
+        path to file, rename filename hashed 'sha256' name
+            type: str
+    """
+    path_to_cover_anime = f"{instance.original_anime_name}/cover/"
+    hashed_filename = file_hashing(instance, file, 'cover_anime')
+    path = os.path.join(path_to_cover_anime, hashed_filename)
+    end_path = file_exist_check(path)
+    if end_path:
+        return end_path
+    return ''
 
 
 def get_path_to_movie(instance, file) -> str:
-    return f"{instance.anime_movie.original_anime_name}/movie/{file}"
+    """
+    receives
+        'instance' is a class model
+            type: class(model class instance)
+        'file' is a filename to customer
+            type: str(filename)
+    manipulation data
+        checked for file existence, if file is not exist file add
+    return
+        path to file, rename filename hashed 'sha256' name
+            type: str
+    """
+    path_to_movie = f"{instance.anime_movie.original_anime_name}/movie/"
+    hashed_filename = file_hashing(instance, file, 'anime_movie_video')
+    path = os.path.join(path_to_movie, hashed_filename)
+    end_path = file_exist_check(path)
+    if end_path:
+        return end_path
+    return ''
 
 
 def get_path_to_episode(instance, file) -> str:
-    return f"{instance.anime.original_anime_name}" \
-           f"/season-{instance.anime_season.season_number}/episode/{file}"
+    """
+    receives
+        'instance' is a class model
+            type: class(model class instance)
+        'file' is a filename to customer
+            type: str(filename)
+    manipulation data
+        checked for file existence, if file is not exist file add
+    return
+        path to file, rename filename hashed 'sha256' name
+            type: str
+    """
+    path_to_episode = f"{instance.anime.original_anime_name}/season-" \
+                      f"{instance.anime_season.season_number}/"
+    hashed_filename = file_hashing(instance=instance, file=file, field='anime_video')
+    path = os.path.join(path_to_episode, hashed_filename)
+    end_path = file_exist_check(path)
+    if end_path:
+        return end_path
+    return ''
 
 
 class OverWriteStorage(FileSystemStorage):
--- api_anime01/settings.py
+++ api_anime01/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'drf_yasg',
     'anime',
     'users',
 ]
