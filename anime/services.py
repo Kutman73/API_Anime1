62600499e29c818bc3f82e3768983c248c362939
Kutman73 <0.0000000002clash@gmail.com>
2023-02-09 21:05:36 +0600

    speeded up file find times by milliseconds, and in terms of performance, these changes may vary depending on the specific use case and the environment in which the code is running

--- anime/services.py
+++ anime/services.py
@@ -2,13 +2,13 @@ from api_anime01 import settings
 import os
 
 
-def base(**kwargs):
+def base(**kwargs) -> list:
     file_path = settings.BASE_DIR / f"media/{kwargs['original_anime_name']}" \
                                     f"/season-{kwargs['season']}" \
                                     f"/episode-{kwargs['episode']}/"
     file_list = []
-    for files in os.listdir(file_path):
-        file = f"{file_path}/{files}"
-        if os.path.isfile(file):
-            file_list.append(f"http://127.0.0.1:8000/media/{file[45:]}")
+    prefix_length = len("/home/kutu/PycharmProjects/api_anime01/media/")
+    for file in os.scandir(file_path):
+        if file.is_file():
+            file_list.append("http://127.0.0.1:8000/media/" + file.path[prefix_length:])
     return file_list
